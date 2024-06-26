cbf78b4578b4928de3f2c49547cb256cc77bceff
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-26 22:50:36 +0300

    Sharlabr: автоматический коммит от 26.06.2024 22:50:36

--- commitbot.sh
+++ commitbot.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 current_date=$(date '+%d.%m.%Y %H:%M:%S')
-git pull
+echo
+echo "Текущее время: $current_date"
+echo "git pull"
+git pull 
 
 if [ "$(git status | grep 'git add')" ]; then
-    echo 'Будем коммитить!'
+    commit_message="Sharlabr: автоматический коммит от $current_date"
+    echo "Коммитим\ngit add -A"
     git add -A
-    git commit -m "Sharlabr: автоматический коммит от $current_date" > /dev/null
+    echo "git commit -m $commit_message"
+    git commit -m "$commit_message"
+    echo "git push"
     git push
 else
-    echo 'Not to commit!'
+    echo 'Коммитить нечего'
 fi
