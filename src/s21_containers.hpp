e659daa2af26b332f965855587b06b410053541b
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-27 20:00:02 +0300

    Sharlabr: автоматический коммит от 27.06.2024 20:00

--- src/s21_containers.hpp
+++ src/s21_containers.hpp
@@ -3,4 +3,17 @@
 
 #include "s21_vector/s21_vector.hpp"
 
+namespace s21 {
+template <typename T>
+class Container {
+ public:
+  using value_type = T;
+  using size_type = std::size_t;
+
+  virtual ~Container() = default;
+  virtual size_type size() const = 0;
+  virtual bool empty() const = 0;
+};
+}  // namespace s21
+
 #endif
