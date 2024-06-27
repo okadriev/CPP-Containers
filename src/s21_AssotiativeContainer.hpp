a53e01aa0365d767ee99160c7ec258623e51030e
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-27 22:12:01 +0300

    sharlabr: Начало ассоциативного контейнера

---
+++ src/s21_AssotiativeContainer.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// #include "s21_containers.hpp"
+#include "s21_pair.hpp"
+#include "s21_rb_tree.cpp"
+
+namespace s21 {
+template <typename T1, typename T2>
+class AssociativeContainer {
+ private:
+  // Самое большое x64 простое число
+  static constexpr size_t _hash_ = 18446744073709551557UL;
+
+ public:
+};
+}  // namespace s21
