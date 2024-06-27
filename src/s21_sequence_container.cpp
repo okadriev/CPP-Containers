26b2706541e99a8c2138a4f7dc6dda2605f40c3f
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-27 20:10:02 +0300

    Sharlabr: автоматический коммит от 27.06.2024 20:10

--- src/s21_sequence_container.cpp
+++ src/s21_sequence_container.cpp
@@ -1,23 +1,18 @@
 #include "s21_sequence_container.hpp"
 
-using namespace s21;
-
+namespace s21 {
 template <typename T>
-s21_sequence_container<T>::s21_sequence_container()
-    : size_(0UL), capacity_(0UL), data_(nullptr) {}
-
-template <typename T>
-s21_sequence_container<T>::s21_sequence_container(size_type n,
-                                                  const T &val = T())
+s21_sequence_container<T>::s21_sequence_container(size_type n, const T &val)
     : size_(n), capacity_(n), data_(n ? new T[n] : nullptr) {
   fill(data_, data_ + n, val);
 }
 
 template <typename T>
-void s21_sequence_container<T>::s21_sequence_container(
+s21_sequence_container<T>::s21_sequence_container(
     const s21_sequence_container<T> &other)
     : size_(other.size_),
       capacity_(other.capacity_),
       data_(new T[other.capacity_]) {
   copy(other.data_, other.data_ + other.size_, data_);
 }
+}  // namespace s21
