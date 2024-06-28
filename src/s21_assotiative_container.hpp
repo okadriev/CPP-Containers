6c9aa72a17ea2a9cf1bfc21629ed8fc793580e19
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-29 00:15:02 +0300

    Sharlabr: автоматический коммит от 29.06.2024 00:15

--- src/s21_assotiative_container.hpp
+++ src/s21_assotiative_container.hpp
@@ -4,26 +4,24 @@
 
 // #include "s21_containers.hpp"
 #include "../s21_set/s21_rb_tree.cpp"
+#include "s21_containers.hpp"
 #include "s21_greater.hpp"
 #include "s21_pair.hpp"
 
 namespace s21 {
-template <typename T1, typename T2>
-class associative_container {
-  using key_t = T1;
-  using value_t = T2;
-  using data_t = pair<key_t, value_t>;
+template <typename Key, typename Value>
+class associative_container : container<rb_tree<pair<Key, Value*>>> {
+  using Tree = rb_tree<pair<Key, Value*>>;
+  using iterator = typename Tree::iterator;
 
  public:
   virtual ~associative_container() = default;
 
-  virtual void insert(const key_t& key, const data_t& value) = 0;
-  virtual void erase(const key_t& key) = 0;
-  virtual bool contains(const key_t& key) const = 0;
-  virtual data_t& at(const key_t& key) = 0;
-  virtual const data_t& at(const key_t& key) const = 0;
+  virtual void insert(const Key& key, const Value& value) = 0;
+  virtual void erase(const Key& key) = 0;
+  virtual bool contains(const Key& key) const = 0;
+  virtual Value& at(const Key& key) = 0;
+  virtual const Value& at(const Key& key) const = 0;
   virtual void clear() = 0;
-  virtual std::size_t size() const = 0;
-  virtual bool empty() const = 0;
 };
 }  // namespace s21
