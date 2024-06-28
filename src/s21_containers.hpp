877c00fba736cdc1b1c79513ca1c7ec3d7ec9b39
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-28 20:58:50 +0300

    Sharlabr: включения для соотвествия ветке develop

--- src/s21_containers.hpp
+++ src/s21_containers.hpp
@@ -1,7 +1,9 @@
 #ifndef S21_CONTAINERS_H_
 #define S21_CONTAINERS_H_
 
-#include <iostream>
+#include "s21_queue/s21_queue.hpp"
+#include "s21_stack/s21_stack.hpp"
+#include "s21_vector/s21_vector.hpp"
 
 // #include "s21_vector/s21_vector.hpp"
 
