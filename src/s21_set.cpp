37d2b82673389ef11cb2529b4ec947977fe18275
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-06-28 20:15:02 +0300

    Sharlabr: автоматический коммит от 28.06.2024 20:15

--- src/s21_set.cpp
+++ src/s21_set.cpp
@@ -1,7 +1,7 @@
 #include <initializer_list>
 #include <iostream>
 
-#include "s21_rb_tree.hpp"
+#include "s21_rb_tree.cpp"
 
 #define PRINT(tree, value)                                           \
   std::cout << "Set " << ((tree.contains(value)) ? "" : "does not ") \
@@ -103,35 +103,34 @@ void set<value_type>::erase(const_reference data) {
 
 using namespace s21;
 
-int main() {
-  set<int> test{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-  set<int> test2{1, 2, 3, 4, 11};
-  set<int> test3(test);
-  // PRINT(test, 11);
-  // test.insert(11);
+// int main() {
+//   set<int> test{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+//   18}; set<int> test2{1, 2, 3, 4, 11}; set<int> test3(test);
+//   // PRINT(test, 11);
+//   // test.insert(11);
 
-  // test.print();
-  test.erase(15);
-  test.erase(14);
-  test.erase(13);
-  test.erase(12);
-  test.erase(4);
-  test.erase(1);
-  test.erase(18);
-  test.erase(17);
-  test.erase(2);
-  test.erase(7);
-  test.erase(16);
-  test.print();
+//   // test.print();
+//   test.erase(15);
+//   test.erase(14);
+//   test.erase(13);
+//   test.erase(12);
+//   test.erase(4);
+//   test.erase(1);
+//   test.erase(18);
+//   test.erase(17);
+//   test.erase(2);
+//   test.erase(7);
+//   test.erase(16);
+//   test.print();
 
-  std::cout << "test " << std::endl;
-  test.print();
-  test2 = test;
-  test2.insert(15);
-  std::cout << "test2 " << std::endl;
-  test2.print();
-  std::cout << "test1 " << std::endl;
-  test.print();
+//   std::cout << "test " << std::endl;
+//   test.print();
+//   test2 = test;
+//   test2.insert(15);
+//   std::cout << "test2 " << std::endl;
+//   test2.print();
+//   std::cout << "test1 " << std::endl;
+//   test.print();
 
-  return 0;
-}
+//   return 0;
+// }
