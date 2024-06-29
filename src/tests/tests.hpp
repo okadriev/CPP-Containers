d67d5ab3ff34277e5ed49b093beba90669c00416
Kristina Miakaeva <92623612+kmiakaeva@users.noreply.github.com>
2024-06-29 15:56:15 +0300

    fix ext for tests

--- src/tests/tests.hpp
+++ src/tests/tests.hpp
@@ -3,8 +3,8 @@
 
 #include <gtest/gtest.h>
 
-#include "../s21_containers.h"
-#include "../s21_containersplus.h"
+#include "../s21_containers.hpp"
+// #include "../s21_containersplus.hpp"
 
 using namespace s21;
 
