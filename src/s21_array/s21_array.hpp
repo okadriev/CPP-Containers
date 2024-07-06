b9cfe6f5d84870fb7010263b53ac48c90546d641
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-07-06 14:15:02 +0300

    Sharlabr: автоматический коммит от 06.07.2024 14:15

--- src/s21_array/s21_array.hpp
+++ src/s21_array/s21_array.hpp
@@ -1,1 +1,51 @@
 #pragma once
+#include <cstddef>
+#include <utility>
+
+namespace s21 {
+
+template <typename T, std::size_t N>
+class array {
+ private:
+  using value_type = T;
+  using pointer = value_type*;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using size_type = std::size_t;
+
+  value_type data_[N] = {};
+
+ public:
+  // Конструктор по умолчанию
+  array() = default;
+
+  // Конструктор для C++17 и выше
+  template <typename... Args, typename = std::enable_if_t<sizeof...(Args) == N>>
+  array(Args&&... args) : data_{std::forward<Args>(args)...} {}
+
+  reference at(size_type index) { return data_[index]; }
+  const_reference at(size_type index) const { return data_[index]; }
+
+  // Оператор индексации
+  reference operator[](size_type index) { return at(index); }
+  const_reference operator[](size_type index) const { return at(index); }
+
+  // Получить размер массива
+  constexpr size_type size() const noexcept { return N; }
+
+  // Итераторы
+  pointer begin() noexcept { return data_; }
+  const pointer begin() const noexcept { return data_; }
+
+  pointer end() noexcept { return data_ + N; }
+  const pointer end() const noexcept { return data_ + N; }
+
+  // Заполнение массива
+  void fill(const T& value) {
+    for (std::size_t i = 0; i < N; ++i) {
+      data_[i] = value;
+    }
+  }
+};
+
+}  // namespace s21
---
+++ src/tests/test_array.cpp
@@ -0,0 +1,131 @@
+#include "s21_array/s21_array.hpp"
+#include "tests.hpp"
+
+TEST(S21ArrayTest, Size) {
+  s21::array<int, 5> arr;
+  EXPECT_EQ(arr.size(), 5);
+}
+
+TEST(S21ArrayTest, EmptyArray) {
+  s21::array<int, 0> arr;
+  EXPECT_EQ(arr.size(), 0);
+  EXPECT_THROW(arr.at(0), std::out_of_range);
+}
+
+TEST(S21ArrayTest, ElementAccess) {
+  s21::array<int, 3> arr;
+  arr[0] = 10;
+  arr[1] = 20;
+  arr[2] = 30;
+
+  EXPECT_EQ(arr[0], 10);
+  EXPECT_EQ(arr[1], 20);
+  EXPECT_EQ(arr[2], 30);
+}
+
+TEST(S21ArrayTest, ElementAccessOutOfBounds) {
+  s21::array<int, 3> arr;
+  EXPECT_THROW(arr.at(3), std::out_of_range);
+  EXPECT_THROW(arr.at(-1), std::out_of_range);  // For signed index type
+}
+
+TEST(S21ArrayTest, AtFunction) {
+  s21::array<int, 3> arr;
+  arr[0] = 10;
+  arr[1] = 20;
+  arr[2] = 30;
+
+  EXPECT_EQ(arr.at(0), 10);
+  EXPECT_EQ(arr.at(1), 20);
+  EXPECT_EQ(arr.at(2), 30);
+  EXPECT_THROW(arr.at(3), std::out_of_range);
+}
+
+TEST(S21ArrayTest, AtFunctionConst) {
+  const s21::array<int, 3> arr = {10, 20, 30};
+
+  EXPECT_EQ(arr.at(0), 10);
+  EXPECT_EQ(arr.at(1), 20);
+  EXPECT_EQ(arr.at(2), 30);
+  EXPECT_THROW(arr.at(3), std::out_of_range);
+}
+
+TEST(S21ArrayTest, FrontBack) {
+  s21::array<int, 3> arr;
+  arr[0] = 10;
+  arr[1] = 20;
+  arr[2] = 30;
+
+  EXPECT_EQ(arr.front(), 10);
+  EXPECT_EQ(arr.back(), 30);
+}
+
+TEST(S21ArrayTest, FrontBackSingleElement) {
+  s21::array<int, 1> arr;
+  arr[0] = 42;
+
+  EXPECT_EQ(arr.front(), 42);
+  EXPECT_EQ(arr.back(), 42);
+}
+
+TEST(S21ArrayTest, Data) {
+  s21::array<int, 3> arr;
+  arr[0] = 10;
+  arr[1] = 20;
+  arr[2] = 30;
+
+  int* data = arr.data();
+  EXPECT_EQ(data[0], 10);
+  EXPECT_EQ(data[1], 20);
+  EXPECT_EQ(data[2], 30);
+}
+
+TEST(S21ArrayTest, DataEmptyArray) {
+  s21::array<int, 0> arr;
+  EXPECT_EQ(arr.data(), nullptr);
+}
+
+TEST(S21ArrayTest, ConstArray) {
+  const s21::array<int, 3> arr = {10, 20, 30};
+  EXPECT_EQ(arr.front(), 10);
+  EXPECT_EQ(arr.back(), 30);
+  EXPECT_EQ(arr[1], 20);
+  EXPECT_EQ(arr.at(2), 30);
+}
+
+TEST(S21ArrayTest, ExceptionSafety) {
+  s21::array<int, 3> arr = {1, 2, 3};
+
+  try {
+    arr.at(3);
+  } catch (const std::out_of_range& e) {
+    EXPECT_STREQ(e.what(), "Index out of range");
+  }
+
+  try {
+    arr.at(4);
+  } catch (const std::out_of_range& e) {
+    EXPECT_STREQ(e.what(), "Index out of range");
+  }
+}
+
+TEST(S21ArrayTest, ModifyArrayThroughData) {
+  s21::array<int, 3> arr = {1, 2, 3};
+  int* data = arr.data();
+  data[0] = 10;
+  data[1] = 20;
+  data[2] = 30;
+
+  EXPECT_EQ(arr[0], 10);
+  EXPECT_EQ(arr[1], 20);
+  EXPECT_EQ(arr[2], 30);
+}
+
+TEST(S21ArrayTest, ConstArrayAccess) {
+  const s21::array<int, 3> arr = {1, 2, 3};
+  const int* data = arr.data();
+
+  EXPECT_EQ(data[0], 1);
+  EXPECT_EQ(data[1], 2);
+  EXPECT_EQ(data[2], 3);
+}
