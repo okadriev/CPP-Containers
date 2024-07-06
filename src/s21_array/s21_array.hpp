19c269041ab1205880e600017849b87b8acccf8c
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-07-06 22:15:31 +0300

    Sharlabr: pre-final array

--- src/s21_array/s21_array.hpp
+++ src/s21_array/s21_array.hpp
@@ -19,31 +19,35 @@ class array {
   data_type data_[N] = {};
 
  public:
-  // Конструктор по умолчанию
   array() = default;
-  // Конструктор через initializer_list
-  array(std::initializer_list<data_type> const&);
-  // Конструктор копирования
-  array(const array&);
-  // Конструктор перемещения
-  array(array&&) noexcept;
+  array(std::initializer_list<data_type> const& items) {
+    for (size_type i = 0; i < N; ++i) data_[i] = items.begin()[i];
+  }
+  array(const array& other) noexcept {
+    for (size_type i = 0; i < N; ++i) data_[i] = other.data_[i];
+  }
+  array(array&& other) noexcept {
+    for (size_type i = 0; i < N; ++i) data_[i] = other.data_[i];
+  }
 
   pointer data() noexcept { return N == 0 ? nullptr : &(data_[0]); }
   const_pointer data() const noexcept { return N == 0 ? nullptr : &(data_[0]); }
 
-  reference front() { return data()[0]; }
-  const_reference front() const { return data()[0]; }
-  reference back() { return data()[N - 1]; }
-  const_reference back() const { return data()[N - 1]; }
-
   reference at(size_type index) {
-    return index < N ? *(data() + index)
-                     : throw std::out_of_range("Index out of range");
+    if (data() == nullptr) throw std::length_error("Array is empty");
+    if (index >= N) throw std::out_of_range("Index out of range");
+    return *(data() + index);
   }
   const_reference at(size_type index) const {
-    return index < N ? *(data() + index)
-                     : throw std::out_of_range("Index out of range");
+    if (data() == nullptr) throw std::length_error("Array is empty");
+    if (index >= N) throw std::out_of_range("Index out of range");
+    return *(data() + index);
   }
+
+  reference front() { return at(0); }
+  const_reference front() const { return at(0); }
+  reference back() { return at(N - 1); }
+  const_reference back() const { return at(N - 1); }
 
   // Оператор индексации
   reference operator[](size_type index) { return at(index); }
@@ -63,5 +67,3 @@ class array {
 };
 
 }  // namespace s21
-
-#include "s21_array_constructors.tpp"
